e706bce7c5bb8d6714bccdd6ea054927839a090d
samuel <samuelsumbayak2000@gmail.com>
2023-07-29 23:41:57 +0700

    menambahkan fitur admin

---
+++ admin/admin.php
@@ -0,0 +1,20 @@
+<!-- Halaman Tambah Data Top Up (tambah_data_topup.php) -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Data Top Up</title>
+</head>
+<body>
+    <h2>Tambah Data Top Up</h2>
+    <form method="post" action="../proses.php">
+        <select name="nama_game" id="nama_game">
+            <option value="Mobile Legends">Mobile Legends</option>
+            <option value="COC">COC</option>
+            <option value="FF">FF</option>
+        </select><br>
+        <input type="number" name="total_dm" placeholder="Total DM" required><br>
+        <input type="number" name="harga" placeholder="Harga" required><br>
+        <input type="submit" value="Tambah Data">
+    </form>
+</body>
+</html>
